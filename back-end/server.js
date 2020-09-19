a9c98cefa42544804b080c75703d6f209586ee67
Joshua Evans <joshevans.tk@gmail.com>
2020-09-19 14:44:40 -0400

    Add prod backend environment

--- back-end/server.js
+++ back-end/server.js
@@ -15,7 +15,6 @@ async function startServer() {
         useUnifiedTopology: true
     });
 
-    // app.use(cors());
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use((req, res, next) => {
